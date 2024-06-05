3061fbb0d9d45a904b8b537bca0f76f2fad8b577
Kairos Cloud <159578967+kairoscloud@users.noreply.github.com>
2024-06-05 07:52:55 -0400

    Update agency-refresh.js

--- agency-refresh.js
+++ agency-refresh.js
@@ -1,42 +1,13 @@
-// GHL Customizer Code
-var head2 = document.getElementsByTagName('head')[0];
-var script2 = document.createElement('script');
-var id2 = Math.random().toString(36).slice(2, 7);
-var src2 = "https://cdn.everypages.com/ghl-customizer/ghl_customizer.php?agency_id=1x762kiz4&id=" + id2;
-script2.setAttribute("id", "ghl-customizer-script");
-script2.src= src2;
-head2.appendChild(script2);
-
-// ThemeBuilder Code
-var head3 = document.getElementsByTagName('head')[0];
-var script3 = document.createElement('script');
-var id3 = Math.random().toString(36).slice(2, 7);
-var src3 = "https://builder.themarketerstoolkit.com/hlembed.js?" + id3;
-script3.setAttribute("id", "themebuilder-script");
-script3.setAttribute("data-agency-id", "1x762kiz4");
-script3.src= src3;
-head3.appendChild(script3);
-
 // Jostens Custom Code
 window.addEventListener('load', function () {
   var group = document.body.classList;
   if (group.contains("jostens") === true) {
-    var head1 = document.getElementsByTagName('head')[0];
-    var script1 = document.createElement('script');
-    var id1 = Math.random().toString(36).slice(2, 7);
-    var src1 = "https://drewderose.github.io/KairosMedia/Jostens/agency-content.js?" + id1;
-    script1.setAttribute("id", "jostens-custom-code");
-    script1.src= src1;
-    head1.appendChild(script1);
-    alert("Jostens Loaded!");
+    var head = document.getElementsByTagName('head')[0];
+    var script = document.createElement('script');
+    var id = Math.random().toString(36).slice(2, 7);
+    var src = "https://kairoscloud.github.io/jostens/agency-content.js?" + id;
+    script.setAttribute("id", "jostens-custom-js");
+    script.src= src;
+    head.appendChild(script);
   }
 })
-
-// Fiverr Custom Code (OFF)
-var head4 = document.getElementsByTagName('head')[0];
-var script4 = document.createElement('script');
-var id4 = Math.random().toString(36).slice(2, 7);
-var src4 = "https://fiverr.ghlbranding.com/drewderose/app.js?" + id4;
-script4.setAttribute("id", "fiverr-custom-code");
-script4.src= src4;
-// head4.appendChild(script4);
