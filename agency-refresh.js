0af573148ca8d8b467cb9d0362e3a029baf4e8ed
Kairos Cloud <159578967+kairoscloud@users.noreply.github.com>
2024-06-05 09:10:20 -0400

    Update agency-refresh.js

--- agency-refresh.js
+++ agency-refresh.js
@@ -1,13 +1,16 @@
+// Random Refresh ID
+window.id = Math.random().toString(36).slice(2, 7);
+
 // GHL Customizer Code
 var customizer = document.createElement('script');
-var src1 = "https://cdn.everypages.com/ghl-customizer/ghl_customizer.php?agency_id=1x762kiz4";
+var src1 = "https://cdn.everypages.com/ghl-customizer/ghl_customizer.php?agency_id=1x762kiz4&id=" + id;
 customizer.setAttribute("id", "ghl-customizer-script");
 customizer.src= src1;
 document.getElementsByTagName('head')[0].appendChild(customizer);
 
 // ThemeBuilder Code
 var themebuilder = document.createElement('script');
-var src2 = "https://builder.themarketerstoolkit.com/hlembed.js";
+var src2 = "https://builder.themarketerstoolkit.com/hlembed.js?" + id;
 themebuilder.setAttribute("id", "themebuilder-script");
 themebuilder.setAttribute("data-agency-id", "1x762kiz4");
 themebuilder.src= src2;
@@ -18,7 +21,7 @@ window.addEventListener('load', function () {
   var group = document.body.classList;
   if (group.contains("jostens") === true) {
     var jostens = document.createElement('script');
-    var src3 = "https://kairoscloud.github.io/jostens/directory.js?";
+    var src3 = "https://kairoscloud.github.io/jostens/directory.js?" + id;
     jostens.setAttribute("id", "jostens-custom-js");
     jostens.src= src3;
     document.getElementsByTagName('head')[0].appendChild(jostens);
@@ -29,7 +32,7 @@ window.addEventListener('load', function () {
 var url = window.location.href.split("/")[5];
 if (url === "owNEzpbrfBjp4weSARXD") {
   var test = document.createElement('script');
-  var src4 = "https://kairoscloud.github.io/main/test-code.js?";
+  var src4 = "https://kairoscloud.github.io/main/test-code.js?" + id;
   test.setAttribute("id", "test-code");
   test.src= src4;
   document.getElementsByTagName('head')[0].appendChild(test);
