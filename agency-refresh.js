d0143428959d97ef22e64af9c8b46e143a73816a
Kairos Cloud <159578967+kairoscloud@users.noreply.github.com>
2024-06-04 22:02:22 -0400

    Update agency-refresh.js

--- agency-refresh.js
+++ agency-refresh.js
@@ -1,16 +1,3 @@
-// Jostens Custom Code
-var parts = (window.location.href).split('/');
-var lastParts = parts[parts.length - 3] + "/" + parts[parts.length - 2] + "/" + parts[parts.length - 1];
-if (lastParts === "contacts/smart_list/All") { // if last part of URL matches "contacts/smart_list/All"
-  var head1 = document.getElementsByTagName('head')[0];
-  var script1 = document.createElement('script');
-  var id1 = Math.random().toString(36).slice(2, 7);
-  var src1 = "https://drewderose.github.io/KairosMedia/Jostens/agency-content.js?" + id1;
-  script1.setAttribute("id", "jostens-custom-code_agency-content");
-  script1.src= src1;
-  head1.appendChild(script1);
-}
-
 // GHL Customizer Code
 var head2 = document.getElementsByTagName('head')[0];
 var script2 = document.createElement('script');
@@ -29,6 +16,19 @@ script3.setAttribute("id", "themebuilder-script");
 script3.setAttribute("data-agency-id", "1x762kiz4");
 script3.src= src3;
 head3.appendChild(script3);
+
+// Jostens Custom Code
+var parts = (window.location.href).split('/');
+var lastParts = parts[parts.length - 3] + "/" + parts[parts.length - 2] + "/" + parts[parts.length - 1];
+if (lastParts === "contacts/smart_list/All") { // if last part of URL matches "contacts/smart_list/All"
+  var head1 = document.getElementsByTagName('head')[0];
+  var script1 = document.createElement('script');
+  var id1 = Math.random().toString(36).slice(2, 7);
+  var src1 = "https://drewderose.github.io/KairosMedia/Jostens/agency-content.js?" + id1;
+  script1.setAttribute("id", "jostens-custom-code_agency-content");
+  script1.src= src1;
+  head1.appendChild(script1);
+}
 
 // Fiverr Custom Code (OFF)
 var head4 = document.getElementsByTagName('head')[0];
