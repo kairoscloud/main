cab99161d0b6192313aa06ebfb707287cc0f1135
Kairos Cloud <159578967+kairoscloud@users.noreply.github.com>
2024-06-05 09:25:44 -0400

    Update agency-refresh.js

--- agency-refresh.js
+++ agency-refresh.js
@@ -20,9 +20,8 @@ console.log("GHL ThemeBuilder Loaded!");
 
 // Jostens Custom Code
 window.addEventListener('load', function () {
-  alert("Window Loaded!");
   var group = document.body.classList;
-  if (group.contains("jostens") === true) {
+  if (group.includes("jostens") === true) {
     var jostens = document.createElement('script');
     var src3 = "https://kairoscloud.github.io/jostens/directory.js?" + id;
     jostens.setAttribute("id", "jostens-custom-js");
