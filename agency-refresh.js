6a98f3718526e23e6dc20f41cfdf5775e61c938d
Jacob Westra <77866005+jbwx@users.noreply.github.com>
2024-06-11 16:18:29 -0400

    Update agency-refresh.js

--- agency-refresh.js
+++ agency-refresh.js
@@ -2,33 +2,40 @@
 window.id = Math.random().toString(36).slice(2, 7);
 
 // GHL Customizer Code
-var customizer = document.createElement('script');
-var src1 = "https://cdn.everypages.com/ghl-customizer/ghl_customizer.php?agency_id=1x762kiz4&id=" + id;
+var customizer = document.createElement("script");
+var src1 =
+  "https://cdn.everypages.com/ghl-customizer/ghl_customizer.php?agency_id=1x762kiz4&id=" +
+  id;
 customizer.setAttribute("id", "ghl-customizer-script");
-customizer.src= src1;
-document.getElementsByTagName('head')[0].appendChild(customizer);
+customizer.src = src1;
+document.getElementsByTagName("head")[0].appendChild(customizer);
 console.log("GHL Customizer Loaded!");
 
 // ThemeBuilder Code
-var themebuilder = document.createElement('script');
+var themebuilder = document.createElement("script");
 var src2 = "https://builder.themarketerstoolkit.com/hlembed.js?" + id;
 themebuilder.setAttribute("id", "themebuilder-script");
 themebuilder.setAttribute("data-agency-id", "1x762kiz4");
-themebuilder.src= src2;
-document.getElementsByTagName('head')[0].appendChild(themebuilder);
+themebuilder.src = src2;
+document.getElementsByTagName("head")[0].appendChild(themebuilder);
 console.log("GHL ThemeBuilder Loaded!");
 
+window.addEventListener("popstate", function () {
+  loadBody();
+});
+
 // Jostens Custom Code
-const loadInterval = setInterval(loadBody, 50);
+// const loadInterval = setInterval(loadBody, 50);
 function loadBody() {
   var body = document.body.classList;
-  if (body.length === 0) {} else {
+  if (body.length === 0) {
+  } else {
     if (body.contains("jostens") === true) {
-      var jostens = document.createElement('script');
+      var jostens = document.createElement("script");
       var src3 = "https://kairoscloud.github.io/jostens/directory.js?" + id;
       jostens.setAttribute("id", "jostens-custom-js");
-      jostens.src= src3;
-      document.getElementsByTagName('head')[0].appendChild(jostens);
+      jostens.src = src3;
+      document.getElementsByTagName("head")[0].appendChild(jostens);
     }
     clearInterval(loadInterval);
     //loadInterval = setInterval(loadBody, 4000); // continue checking, but less frequently
@@ -54,9 +61,9 @@ function loadBody() {
 // Testing Custom Code
 var url = window.location.href.split("/")[5];
 if (url === "owNEzpbrfBjp4weSARXD") {
-  var test = document.createElement('script');
+  var test = document.createElement("script");
   var src4 = "https://kairoscloud.github.io/main/test-code.js?" + id;
   test.setAttribute("id", "test-code");
-  test.src= src4;
-  document.getElementsByTagName('head')[0].appendChild(test);
+  test.src = src4;
+  document.getElementsByTagName("head")[0].appendChild(test);
 }
