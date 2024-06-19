a60144a4133a66dd0b4ea2ffdc81a3df94a47b69
Jacob Westra <77866005+jbwx@users.noreply.github.com>
2024-06-19 17:36:09 -0400

    Update newLocation.js

--- newLocation.js
+++ newLocation.js
@@ -4,10 +4,6 @@
     //  - download list of fb locs
     //  - check if loc exists in list
     //  - if not, add entry to firebase with a new token for that location. Redownload list
-    //  - check if URL has changed every 5 seconds (not urgent). If so, rerun everything
-
-    console.log("Initializing newloc!");
-    let globalLocationList = [];
 
     let firebaseConfig = {
         apiKey: "AIzaSyAkvl6HKgup1AofIrUU_Q7b4RlvhI2QTpc",
@@ -22,26 +18,18 @@
     let name1 = "primary" + Math.random();
     let name2 = "secondary" + Math.random();
 
-    firebase.initializeApp(firebaseConfig, "primary");
-    firebase.initializeApp(firebaseConfig, "secondary"); // Use a different name for a new instance, this forces it to update
+    firebase.initializeApp(firebaseConfig, name1);
+    firebase.initializeApp(firebaseConfig, name2); // Use a different name for a new instance, this forces it to update
+
+    console.log("Initializing newloc!");
+    let globalLocationList = [];
 
     let prevLocationNL = "";
-    let locationCheckInterval = setInterval(detectLocationChange, 5000);
-
-    function detectLocationChange() {
-        // let currentLocationNL = window.location.href.split("/")[5];
-        let currentLocationNL = "vPGRw179FP3xMUXHoDWF"; // for testing
-        if (currentLocationNL != prevLocationNL) {
-            // if location changed
-            checkIfLocationInFB();
-        }
-        prevLocationNL = currentLocationNL;
-    }
 
     function checkIfLocationInFB() {
         // we encapsulate it in a function, so it doesn't interfere with firebase stuff in any other files
 
-        let firestore = firebase.app("primary").firestore();
+        let firestore = firebase.app(name1).firestore();
         // let locationNL = window.location.href.split("/")[5];
         let locationNL = "vPGRw179FP3xMUXHoDWF"; // for testing
         let agencyTokenNL = "";
@@ -113,7 +101,7 @@
         setTimeout(() => {
             // allow a 1s delay for firebase to update with the new entry
             firebase
-                .app("secondary")
+                .app(name2)
                 .firestore()
                 .collection("tokens")
                 .get()
