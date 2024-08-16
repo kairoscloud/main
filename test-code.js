980184ca7f9acb8bd08b2dbbe9a144729bfbea46
Jacob Westra <77866005+jbwx@users.noreply.github.com>
2024-08-16 14:27:38 -0400

    Update test-code.js

--- test-code.js
+++ test-code.js
@@ -4,10 +4,12 @@ console.log("Script ver 2");
 loaded["test"] = true;
 let test_code_var = true;
 let throwError = false;
-setInterval(() => {
+let tempIntv = setInterval(() => {
   console.log("Test script running!");
   active["test"] = Date.now();
   if (throwError) {
     throw new Error();
   }
 }, 2000);
+
+setTimeout(clearInterval(tempIntv), 10000); // clear after 10s
