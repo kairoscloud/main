fa6d3244b64473bdca5ab74b3ae1073384fb8a1d
Jacob Westra <77866005+jbwx@users.noreply.github.com>
2024-08-16 13:39:37 -0400

    Update test-code.js

--- test-code.js
+++ test-code.js
@@ -1,6 +1,8 @@
 console.log("Test Code Loaded!");
 
+loaded["test"] = true;
 test_code_var = true;
- setInterval(() => {
-        console.log("Test script running!")
-}, 3000);
+setInterval(() => {
+  console.log("Test script running!");
+  active["test"] = Date.now();
+}, 2000);
