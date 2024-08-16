3345868c198e0371fd084075c5066fa44993b35b
Jacob Westra <77866005+jbwx@users.noreply.github.com>
2024-08-16 13:51:35 -0400

    Update test-code.js

--- test-code.js
+++ test-code.js
@@ -1,5 +1,5 @@
 console.log("Test Code Loaded!");
-Console.log("Script ver 2");
+console.log("Script ver 2");
 
 loaded["test"] = true;
 let test_code_var = true;
@@ -7,7 +7,7 @@ let throwError = false;
 setInterval(() => {
   console.log("Test script running!");
   active["test"] = Date.now();
-  if(throwError){
-    throw new Error;
+  if (throwError) {
+    throw new Error();
   }
 }, 2000);
