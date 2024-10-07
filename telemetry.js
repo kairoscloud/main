89fcb54ff32ce01eef455bd93acd2b975bb6045d
Jacob Westra <77866005+jbwx@users.noreply.github.com>
2024-10-06 23:41:28 -0400

    Update telemetry.js

--- telemetry.js
+++ telemetry.js
@@ -4,12 +4,15 @@
 //   - It creates an entry in firebase (if not already exists), with a unique telemetry ID
 //   - It populates the entry with every single piece of data it can get from the browser
 //  Every 2 minutes:
+//    - Logs all global variables
 //    - Logs timestamp
 //    - Logs all currently-active scripts + versions
 //    - Logs the current URL
 //    - Pushes the logStack to Firebase, after turning it into a string and separating it by newlines
 
 console.log("Telemetry loaded!");
+
+let foundUsername = "Could not find username";
 
 const browserInfo = getBrowserInfo();
 
@@ -41,7 +44,7 @@ window.telemetryID =
       currentDay,
   );
 
-// format: browser-version-platform-123456
+// format: browser-platform-123456
 console.log("Telemetry ID: " + telemetryID);
 
 const userTelemetryData = {
@@ -117,8 +120,6 @@ function telemetryGetCurrentDay() {
   return `${month}-${day}-${year}`;
 }
 
-// update the telemetry entry every 2 minutes
-//function initializeInterval() {
 let telemetryInterval = setInterval(() => {
   // we need to wrap this in a try-catch: it runs every 2 minutes, and we can't afford for it to break the entire app
   try {
@@ -126,7 +127,7 @@ let telemetryInterval = setInterval(() => {
       lastUpdated: Date.now(),
       consoleLog: formatLogStack(),
       locationID: window.location.href.split("/")[5] || "No location ID",
-      userName: getUsername(),
+      userName: foundUsername,
     });
   } catch (error) {
     console.log(
@@ -134,7 +135,6 @@ let telemetryInterval = setInterval(() => {
     );
   }
 }, 120000); // 60s * 2 * 1000ms
-//}
 
 function formatLogStack() {
   window.logStack.push(
@@ -162,18 +162,44 @@ function formatLogStack() {
   //     window.logStack.push(variable + ": " + valueOf(variable)); // Logs only variables you've added
   //   }
   // }
-  return window.logStack.join("\n");
+  return window.logStack.join(" //NL// "); // our specialized newline enumerator, since \n doesn't work in Firestore
 }
 
-function getUsername() {
-  let userNameContainer = document.querySelector(
-    "#app > div:nth-child(1) > div.flex.v2-collapse.sidebar-v2-location.pmd-app." +
-      window.location.href.split("/")[5] +
-      ".flex.v2-collapse.sidebar-v2-location > div.error-alert > header > div.container-fluid > div.hl_header--controls > div.hl_header--dropdown.dropdown.--no-caret.show > div > div.mx-4.my-1.user-info-card > div > div.inline-block.w-56.px-2.py-1.text-sm.break-all.dark\\:text-white > div.text-gray-900",
-  );
-  if (userNameContainer) {
-    return userNameContainer.innerText;
-  } else {
-    return "Couldn't find username";
+globalWaitForElement(
+  "#app > div:nth-child(1) > div.flex.v2-collapse.sidebar-v2-location.pmd-app." +
+    window.location.href.split("/")[5] +
+    ".flex.v2-collapse.sidebar-v2-location > div.error-alert > header > div.container-fluid > div.hl_header--controls > div.hl_header--dropdown.dropdown.--no-caret.show > div > div.mx-4.my-1.user-info-card > div > div.inline-block.w-56.px-2.py-1.text-sm.break-all.dark\\:text-white > div.text-gray-900",
+  false,
+  function (element) {
+    foundUsername = element.innerText;
+  },
+);
+
+function globalWaitForElement(query, continuous, callback) {
+  console.log("Listening for element '" + query + "'...");
+  const observer = new MutationObserver(() => {
+    const element = document.querySelector(query);
+    // if exists, and if not already modified
+    if (element && !element.hasAttribute("cScriptModified")) {
+      element.setAttribute("cScriptModified", true); // mark as modified
+      if (!continuous) {
+        observer.disconnect();
+      }
+      console.log("Found element '" + query + "'");
+      callback(element); // call the callback function with found element as arg
+    }
+  });
+
+  observer.observe(document.body, { childList: true, subtree: true });
+
+  // Initial check in case the element is already present
+  const element = document.querySelector(query);
+  if (element && !element.hasAttribute("cScriptModified")) {
+    element.setAttribute("cScriptModified", true);
+    if (!continuous) {
+      observer.disconnect();
+    }
+    console.log("Found element '" + query + "'");
+    callback(element);
   }
 }
