2cbb0eb1d90849b2ff22acbf8d2b1d9808391126
Kairos Cloud <159578967+kairoscloud@users.noreply.github.com>
2026-02-20 16:42:01 -0500

    Apply only to Jostens Demo

--- telemetry.js
+++ telemetry.js
@@ -9,32 +9,34 @@ window.logStack = [];
     originalLog.apply(console, args);
   }));
 
-setInterval(
-  () => {
-    if (window.logStack.length === 0) {
-      return;
-    }
-    const logsToSend = [...window.logStack];
-    const params = new URLSearchParams(
-      document.currentScript.src.split("?")[1],
-    );
-    console.log("Sending telemetry logs...");
-    const source = params.get("ref");
-    window.logStack = [];
-    fetch(
-      "https://logging-xxvu4qktma-uc.a.run.app?source=telemetry:" + source,
-      {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json",
+if (window.location.href.includes("owNEzpbrfBjp4weSARXD")) {
+  setInterval(
+    () => {
+      if (window.logStack.length === 0) {
+        return;
+      }
+      const logsToSend = [...window.logStack];
+      const params = new URLSearchParams(
+        document.currentScript.src.split("?")[1],
+      );
+      console.log("Sending telemetry logs...");
+      const source = params.get("ref");
+      window.logStack = [];
+      fetch(
+        "https://logging-xxvu4qktma-uc.a.run.app?source=telemetry:" + source,
+        {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify({ entries: logsToSend }),
         },
-        body: JSON.stringify({ entries: logsToSend }),
-      },
-    ).catch((error) => {
-      // If there's an error sending logs, put them back in the stack to try again later
-      window.logStack.unshift(...logsToSend);
-      originalLog("Error sending logs:", error);
-    });
-  },
-  5 * 60 * 1000,
-);
+      ).catch((error) => {
+        // If there's an error sending logs, put them back in the stack to try again later
+        window.logStack.unshift(...logsToSend);
+        originalLog("Error sending logs:", error);
+      });
+    },
+    5 * 60 * 1000,
+  );
+}
